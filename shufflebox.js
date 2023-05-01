53e1d3b1ee2c327ca93cf2ea5591bd7ae5fe02e1
JoeToscani <39446512+Episode353@users.noreply.github.com>
2023-05-01 14:27:45 -0400

    BEFORE HEADER UPDATE

---
+++ shufflebox.js
@@ -0,0 +1,15 @@
+$(function () {
+    var files = [
+        'pages/html-shuffle/gunt-fish.html',
+        'pages/html-shuffle/builder-dog.html',
+        'pages/html-shuffle/blender-fish.html',
+        'pages/html-shuffle/rat-night.html',
+        'pages/html-shuffle/4chan_idiot.html',
+        'pages/html-shuffle/wizard-ask.html',
+        'pages/html-shuffle/seep-rule.html',
+        'pages/html-shuffle/joes-basement-countdown.html'
+    ];
+
+    var randomFile = files[Math.floor(Math.random() * files.length)]; // Pick a random file
+    $("#htmlshufflebox").load(randomFile); // Load the random file into the div
+});
