dac6456d839c41802e778aa60b37f5553ea2ddd6
JoeToscani <39446512+Episode353@users.noreply.github.com>
2023-05-04 07:52:15 -0400

    added gallery to shufflebox, but doesnt work

--- scripts/shufflebox.js
+++ scripts/shufflebox.js
@@ -1,16 +1,24 @@
 $(function () {
     var files = [
-        'pages/html-shuffle/gunt-fish.html',
-        'pages/html-shuffle/builder-dog.html',
-        'pages/html-shuffle/blender-fish.html',
-        'pages/html-shuffle/rat-night.html',
+       'pages/html-shuffle/gunt-fish.html',
+       'pages/html-shuffle/builder-dog.html',
+       'pages/html-shuffle/blender-fish.html',
+       'pages/html-shuffle/rat-night.html',
         'pages/html-shuffle/4chan_idiot.html',
-        'pages/html-shuffle/wizard-ask.html',
+       'pages/html-shuffle/wizard-ask.html',
         'pages/html-shuffle/seep-rule.html',
         'pages/html-shuffle/franken-doodle-episode.html',
         'pages/html-shuffle/guiseppe.html',
+        'pages/html-shuffle/gallery.html',
         'pages/html-shuffle/joes-basement-countdown.html'
     ];
+
+    // Use the current time in milliseconds as a seed for the random number generator
+    var seed = new Date().getTime();
+    var random = function () {
+        var x = Math.sin(seed++) * 10000;
+        return x - Math.floor(x);
+    };
 
     // Use the current time in milliseconds as a seed for the random number generator
     var seed = new Date().getTime();
