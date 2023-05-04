c9a2ccb07a5696d1b899c60fe574bd67cc2047de
JoeToscani <39446512+Episode353@users.noreply.github.com>
2023-05-03 21:25:49 -0400

    added board page

---
+++ scripts/board.js
@@ -0,0 +1,161 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let currentColor = 'black';
+let currentBackgroundColor = 'white';
+let currentBrushSize = 10;
+let currentTool = 'brush';
+
+const undoButton = document.getElementById('undo');
+const undoStack = [];
+let initialCanvasState = ctx.getImageData(0, 0, canvas.width, canvas.height);
+undoStack.push(initialCanvasState);
+
+undoButton.addEventListener('click', () => {
+    if (undoStack.length > 1) {
+        ctx.putImageData(undoStack.pop(), 0, 0);
+    } else if (undoStack.length === 1) {
+        ctx.putImageData(undoStack[0], 0, 0);
+        undoStack.length = 0;
+    }
+});
+
+
+
+// makes backround white on startup
+
+ctx.fillStyle = currentBackgroundColor;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// transparent button makes transparant backround
+
+const transparentBackgroundButton = document.getElementById('transparent-background');
+transparentBackgroundButton.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+// Add the current canvas state to the undo stack
+
+function addToUndoStack() {
+    const currentCanvasState = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    if (!isEqualImageData(previousCanvasState, currentCanvasState)) {
+        undoStack.push(currentCanvasState);
+        previousCanvasState = currentCanvasState;
+    }
+    // remove excess undo steps
+    if (undoStack.length > maxUndoSteps) {
+        undoStack.shift();
+    }
+}
+
+function isEqualImageData(imageData1, imageData2) {
+    if (imageData1.width !== imageData2.width || imageData1.height !== imageData2.height) {
+        return false;
+    }
+    for (let i = 0; i < imageData1.data.length; i++) {
+        if (imageData1.data[i] !== imageData2.data[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Add the addToUndoStack function to the mouseup event listener
+canvas.addEventListener('mouseup', () => {
+    if (isDrawing) {
+        addToUndoStack();
+        isDrawing = false;
+    }
+});
+
+// Set up the clear page button
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', () => {
+    undoStack.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+    ctx.fillStyle = currentBackgroundColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
+
+// Set up the color picker
+const colorPicker = document.getElementById('color');
+colorPicker.addEventListener('change', (event) => {
+    currentColor = event.target.value;
+});
+
+// Set up the background color picker
+const backgroundColorPicker = document.getElementById('background-color');
+backgroundColorPicker.addEventListener('input', (event) => {
+    currentBackgroundColor = event.target.value;
+    ctx.fillStyle = currentBackgroundColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
+
+
+// Set up the brush/eraser size slider
+const brushSizeSlider = document.getElementById('brush-size');
+brushSizeSlider.addEventListener('input', (event) => {
+    currentBrushSize = event.target.value;
+});
+
+// Set up the eraser button
+const eraserButton = document.getElementById('eraser');
+eraserButton.addEventListener('click', () => {
+    currentTool = 'eraser';
+});
+
+// Set up the brush button
+const brushButton = document.getElementById('brush');
+brushButton.addEventListener('click', () => {
+    currentTool = 'brush';
+});
+
+// Set up the canvas size input elements
+const canvasWidthInput = document.getElementById('canvas-width');
+const canvasHeightInput = document.getElementById('canvas-height');
+canvasWidthInput.addEventListener('change', () => {
+    canvas.width = canvasWidthInput.value;
+    ctx.fillStyle = currentBackgroundColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
+canvasHeightInput.addEventListener('change', () => {
+    canvas.height = canvasHeightInput.value;
+    ctx.fillStyle = currentBackgroundColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
+
+// Add the mouse event listeners
+canvas.addEventListener('mousedown', (event) => {
+    isDrawing = true;
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+});
+
+canvas.addEventListener('mousemove', (event) => {
+    if (!isDrawing) return;
+    const x = event.offsetX;
+    const y = event.offsetY;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(x, y);
+    ctx.strokeStyle = currentTool === 'brush' ? currentColor : currentBackgroundColor;
+    ctx.lineWidth = currentBrushSize;
+    ctx.lineCap = 'round';
+    ctx.stroke();
+    lastX = x;
+    lastY = y;
+});
+
+// Move the mouseup and mouseout events to the window object
+window.addEventListener('mouseup', () => {
+    if (isDrawing) {
+        undoStack.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+        isDrawing = false;
+    }
+});
+
+window.addEventListener('mouseout', () => {
+    isDrawing = false;
+});
