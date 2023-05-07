57deb50a077cf13cd32a822572a75c29b988e100
JoeToscani <39446512+Episode353@users.noreply.github.com>
2023-05-06 21:47:45 -0400

    fixed htmlshufflebox

--- scripts/shufflebox.js
+++ scripts/shufflebox.js
@@ -1,17 +1,17 @@
 $(function () {
     var files = [
-       // 'pages/html-shuffle/gunt-fish.html',
-      //  'pages/html-shuffle/builder-dog.html',
-      //  'pages/html-shuffle/blender-fish.html',
-      //  'pages/html-shuffle/rat-night.html',
-       // 'pages/html-shuffle/4chan_idiot.html',
-       // 'pages/html-shuffle/wizard-ask.html',
-       // 'pages/html-shuffle/seep-rule.html',
-       // 'pages/html-shuffle/franken-doodle-episode.html',
-       // 'pages/html-shuffle/guiseppe.html',
-       // 'pages/html-shuffle/gallery-shuffle.html',
-        'pages/html-shuffle/feesh-brain.html',
-       // 'pages/html-shuffle/joes-basement-countdown.html'
+       'pages/html-shuffle/gunt-fish.html',
+       'pages/html-shuffle/builder-dog.html',
+       'pages/html-shuffle/blender-fish.html',
+       'pages/html-shuffle/rat-night.html',
+       'pages/html-shuffle/4chan_idiot.html',
+       'pages/html-shuffle/wizard-ask.html',
+       'pages/html-shuffle/seep-rule.html',
+       'pages/html-shuffle/franken-doodle-episode.html',
+       'pages/html-shuffle/guiseppe.html',
+       'pages/html-shuffle/gallery-shuffle.html',
+       'pages/html-shuffle/feesh-brain.html',
+       'pages/html-shuffle/joes-basement-countdown.html'
     ];
 
     // Use the current time in milliseconds as a seed for the random number generator
