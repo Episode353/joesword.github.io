dd4a6bd0086c52ee23b9f1a7db3b960c0297a9c7
JoeToscani <39446512+Episode353@users.noreply.github.com>
2023-05-12 14:52:55 -0400

    Itemspinner-firebase pulls from firebase database

---
+++ scripts/back-map.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+document.addEventListener("keydown", function (event) {
+    if (event.altKey && event.key === "p") {
+        window.open("../pages/back-map.html");
+    }
+});
