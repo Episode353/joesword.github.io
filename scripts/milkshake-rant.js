66682c7e9452da5f3ebbf3ee5fbf51afbe9416b6
JoeToscani <39446512+Episode353@users.noreply.github.com>
2023-05-29 14:25:02 -0400

    ADDED ralph review

---
+++ scripts/milkshake-rant.js
@@ -0,0 +1,64 @@
+WARNING: RANT AHEAD! Brace yourself for the biggest straw induced disaster that has ever befallen the milkshake drinking world.
+
+Prepare to witness the ultimate strawspiracy against all milkshake enthusiasts.
+
+        Join me on this infuriating journey as we delve into the depths of a tragedy so catastrophic that it defies belief.
+
+        Ladies and gentlemen, welcome to the tale of the milkshake with a straw so narrow it rendered it utterly undrinkable!
+
+Upon entering the establishment, I was brimming with anticipation.I ordered what appeared to be a luscious, creamy milkshake -
+
+    a treat to appease my sweet tooth and offer respite from the sweltering heat.
+
+    Little did I know that this milkshake would soon become the bane of my existence, my worst nightmare, and a personal affront to everything good and pure in this world.
+
+
+As the milkshake arrived, the first visual impression was satisfactory.
+
+It stood tall and proud, a beacon of hope amidst the chaos.But alas, looks can be deceiving.
+
+When I grabbed the straw and attempted to take my first sip, I quickly realized the magnitude of the atrocity committed against humanity.
+
+The straw was nothing short of an insult, a mere thread of plastic struggling to sustain my insatiable thirst.
+
+With each futile attempt to extract the divine nectar from the depths of the milkshake, the straw would crumble like a weakling, bending under the slightest pressure.
+
+It was as if the universe conspired against me, ensuring that every sip was an exercise in sheer frustration and disappointment.Oh, the anguish! The disappointment! The sheer injustice of it all!
+
+To say that the straw was narrow would be an understatement of galactic proportions.
+
+It was so slender that I questioned its purpose in life.Did it aspire to be a toothpick, a decorative ornament, or merely a cruel joke played by sadistic milkshake vendors ?
+
+    It had no business masquerading as a vessel for liquid consumption.
+
+This abomination of a straw is a betrayal to all milkshake aficionados.
+
+It hampers the very essence of the milkshake experience - the joy of indulging in a thick, creamy concoction that dances across your taste buds.
+
+How can one savor the delightful medley of flavors when the straw cannot deliver the milkshake to its rightful destination ?
+
+    Furthermore, this insult to milkshake lovers everywhere raises the question of intent.
+
+    Was this a deliberate act to undermine the sanctity of milkshakes ? Are we, the milkshake devotees, unworthy of a properly designed straw ?
+
+    Is this some twisted scheme to force us to abandon milkshakes altogether and embrace the tyranny of spoon - driven consumption ?
+
+    I demand justice! I beseech the milkshake industry to rectify this grave oversight.
+
+        We need straws that can handle the mighty task of conquering thick milkshake realms.
+
+        We require robust, industrial - strength straws capable of defying gravity, bending with resilience, and guiding the milkshake to our parched lips.
+
+        No longer shall we tolerate these flimsy, inadequate drinking apparatuses!
+
+In conclusion, my encounter with the milkshake and its abysmal straw has left an indelible mark on my soul.
+
+It is a wound that may never heal, a betrayal I shall never forget.
+
+Until the day when straws of adequate width are universally embraced, I shall carry this burden, warning others of the treacherous strawspiracy that lurks in the world of milkshakes.
+
+Let my tragic tale serve as a rallying cry for all milkshake enthusiasts, urging them to rise up and demand justice for our liquid confections!
+
+May the day come when we can relish in the sweet satisfaction of a milkshake, unburdened by the shackles of an inadequate straw.
+
+Until then, my friends, tread carefully when ordering a milkshake, for you may find yourself sucked into the vortex of straw - induced anguish from which there is no escape.
