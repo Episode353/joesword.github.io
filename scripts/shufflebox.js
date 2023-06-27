24b281456f707debfbf34faa24e1feee4fe7076e
JoeToscani <39446512+Episode353@users.noreply.github.com>
2023-06-27 16:43:32 -0400

    added to shufflebox

--- scripts/shufflebox.js
+++ scripts/shufflebox.js
@@ -1,27 +1,31 @@
 $(function () {
     var files = [
-       'pages/html-shuffle/gunt-fish.html',
-       'pages/html-shuffle/builder-dog.html',
-       'pages/html-shuffle/blender-fish.html',
-       'pages/html-shuffle/rat-night.html',
-       'pages/html-shuffle/4chan_idiot.html',
-       'pages/html-shuffle/wizard-ask.html',
-       'pages/html-shuffle/seep-rule.html',
-       'pages/html-shuffle/franken-doodle-episode.html',
-       'pages/html-shuffle/guiseppe.html',
-       'pages/html-shuffle/gallery-shuffle.html',
-       'pages/html-shuffle/feesh-brain.html',
-       'pages/html-shuffle/fish-plan.html',
-       'pages/html-shuffle/stolen.html',
-       'pages/html-shuffle/dorito-time.html',
-       'pages/html-shuffle/loud-ward.html',
-       'pages/html-shuffle/drake-trash.html',
-       'pages/html-shuffle/arizona.html',
-       'pages/html-shuffle/sigma-grintset.html',
-       'pages/html-shuffle/stole-inch.html',
-       'pages/html-shuffle/jig-dirsty.html',
-       'pages/html-shuffle/mortified_rugby.html'
-
+        'pages/html-shuffle/gunt-fish.html',
+        'pages/html-shuffle/builder-dog.html',
+        'pages/html-shuffle/blender-fish.html',
+        'pages/html-shuffle/rat-night.html',
+        'pages/html-shuffle/4chan_idiot.html',
+        'pages/html-shuffle/wizard-ask.html',
+        'pages/html-shuffle/seep-rule.html',
+        'pages/html-shuffle/franken-doodle-episode.html',
+        'pages/html-shuffle/guiseppe.html',
+        'pages/html-shuffle/gallery-shuffle.html',
+        'pages/html-shuffle/feesh-brain.html',
+        'pages/html-shuffle/fish-plan.html',
+        'pages/html-shuffle/stolen.html',
+        'pages/html-shuffle/dorito-time.html',
+        'pages/html-shuffle/loud-ward.html',
+        'pages/html-shuffle/drake-trash.html',
+        'pages/html-shuffle/arizona.html',
+        'pages/html-shuffle/sigma-grintset.html',
+        'pages/html-shuffle/stole-inch.html',
+        'pages/html-shuffle/man-eat-fish.html',
+        'pages/html-shuffle/brough.html',
+        'pages/html-shuffle/tornado_courage.html',
+        'pages/html-shuffle/sky_error.html',
+        'pages/html-shuffle/jig-dirsty.html',
+        'pages/html-shuffle/mortified_rugby.html'
+       
     ];
 
     // Use the current time in milliseconds as a seed for the random number generator
