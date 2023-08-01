c5e64e325a41b75cb67abbf822231c666a104495
JoeToscani <39446512+Episode353@users.noreply.github.com>
2023-08-01 16:00:16 -0400

    added seep coins

--- scripts/seppe.js
+++ scripts/seppe.js
@@ -1,21 +1,31 @@
-// Wait for the DOM to be ready
 document.addEventListener("DOMContentLoaded", function (event) {
-    // Find all the text nodes on the page
-    var textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+    // Find all <p> and <li> elements on the page
+    var elements = document.querySelectorAll("p, li");
 
-    // Loop through each text node
-    for (var i = 0; i < textNodes.snapshotLength; i++) {
-        var node = textNodes.snapshotItem(i);
-        var text = node.textContent;
+    // Loop through each element
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var preserveWord = element.getAttribute("data-preserve");
+        var text = element.textContent;
 
-        // Replace instances of "seppe", "guiseppe", and "seep" with the formatted version
-        var newText = text.replace(/\b(seppe|guiseppe|seep)\b/gi, '<span class="word-seppe">$1</span>');
+        // Only proceed if the "data-preserve" attribute is not present or is set to "false"
+        if (preserveWord !== "true") {
+            // Replace instances of "seppe", "guiseppe", and "seep" with the formatted version
+            var newText = text.replace(/\b(seppe|guiseppe|seep)\b/gi, '<span class="word-seppe">$1</span>');
 
-        // Update the text node if necessary
-        if (newText !== text) {
-            var newNode = document.createElement("span");
-            newNode.innerHTML = newText;
-            node.parentNode.replaceChild(newNode, node);
+            // Update the element if necessary
+            if (newText !== text) {
+                element.innerHTML = newText;
+            }
         }
     }
 });
+
+
+/*
+
+                <p>Here is a normal sentence with the word "seppe".</p>
+                <p data-preserve="true">This sentence contains "seppe" and should not be changed.</p>
+                <p>Another sentence with the word "seppe".</p>
+
+*/
